3a774a8f94f2e37060e2b45f795c5238b9ce1000
rahmanlana <rahmanlana044@gmail.com>
2026-02-14 23:07:20 +0700

    first commit

---
+++ app/View/Components/Sidebar.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Sidebar extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public $links;
+    public function __construct()
+    {
+        $this->links = [
+            [
+                'label' => 'Dashboard Analitik',
+                'route' => 'home',
+                'is_active' => request()->routeIs('home'),
+                'icon' => 'fas fa-chart-line',
+                'is_dropdown' => false
+            ]
+        ];
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.sidebar');
+    }
+}
---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,56 @@
+<div class="sidebar" data-background-color="white">
+    <div class="sidebar-logo">
+        <!-- Logo Header -->
+        <div class="logo-header" data-background-color="white">
+            <a href="/home" class="logo">
+                <img src="{{ asset('template') }}/assets/img/kaiadmin/logo_light.svg" alt="navbar brand"
+                    class="navbar-brand" height="20" />
+            </a>
+            <div class="nav-toggle">
+                <button class="btn btn-toggle toggle-sidebar">
+                    <i class="gg-menu-right"></i>
+                </button>
+                <button class="btn btn-toggle sidenav-toggler">
+                    <i class="gg-menu-left"></i>
+                </button>
+            </div>
+            <button class="topbar-toggler more">
+                <i class="gg-more-vertical-alt"></i>
+            </button>
+        </div>
+        <!-- End Logo Header -->
+    </div>
+    <div class="sidebar-wrapper scrollbar scrollbar-inner">
+        <div class="sidebar-content">
+            <ul class="nav nav-secondary">
+                @foreach ($links as $link)
+                    @if ($link['is_dropdown'])
+                        <li class="nav-item {{$link['is_active'] ? 'active' : ''}}">
+                            <a data-bs-toggle="collapse" href="#dashboard" class="collapsed" aria-expanded="false">
+                                <i class="{{$link['icon']}}"></i>
+                                <p>{{ $links['label'] }}</p>
+                                <span class="caret"></span>
+                            </a>
+                            <div class="collapse" id="dashboard">
+                                <ul class="nav nav-collapse">
+                                    <li>
+                                        <a href="../demo1/index.html">
+                                            <span class="sub-item">Dashboard 1</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                    @else
+                        <li class="nav-item {{$link['is_active'] ? 'active' : ''}}">
+                            <a href="{{route($link['route'])}}">
+                                <i class="{{$link['icon']}}"></i>
+                                <p>{{$link['label']}}</p>
+                            </a>
+                        </li>
+                    @endif
+                @endforeach
+            </ul>
+        </div>
+    </div>
+</div>
